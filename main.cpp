ebb91515bb34ec14f7f3969f1a430490ff5f41b2
teo-101 <120208346+teo-101@users.noreply.github.com>
2022-12-09 19:46:09 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,15 +6,15 @@ using namespace std;
 
 int main()
 {   cout<<setw(75);
-    cout<<"~Rezolvarea ecuatiilor de gradul 2~"<<endl;  system("pause");
-    cout<<"Introdu coeficienti de forma ax^2+bx+c=0"<<endl;
+    cout<<"~Equation of the second degree~"<<endl;  system("pause");
+    cout<<"Enter the coefficients for ax^2+bx+c=0"<<endl;
     float a,b,c;
     cin>>a>>b>>c;
     float Delta=b*b-4*a*c;
     if (Delta<0)
     { 
         system("chcp 65001");   system ("cls");
-        cout<<"Nu exista valoare pt x din R, deoarece Δ="<<Delta<<endl;
+        cout<<"No value for x from R Δ="<<Delta<<endl;
         system("pause");
         return 0;
     }
